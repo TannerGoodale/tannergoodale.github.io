4826da82adbac13d73ec73c4140f460d7e3a4dcf
Tanner Goodale <49888850+TannerGoodale@users.noreply.github.com>
2019-06-22 20:27:45 -0500

    week 9 JSON home page 1.0

--- assignments/lesson-9/scripts/json.js
+++ assignments/lesson-9/scripts/json.js
@@ -26,17 +26,38 @@ var request = new XMLHttpRequest();
                 var p2 = document.createElement("p");
                 var p3 = document.createElement("p");
 
+                article.setAttribute("class", "jar");
+                h2.setAttribute("class", "jh2");
+                h3.setAttribute("class", "jh3");
+                p1.setAttribute("class", "jp");
+                p2.setAttribute("class", "jp");
+                p3.setAttribute("class", "jp");
+
                 h2.textContent = towns[i].name;
                 h3.textContent = towns[i].motto;
-                p1.textContent = "Year Founded " + towns[i].yearFounded;
-                p2.textContent = "Current Population " + towns[i].currentPopulation;
-                p3.textContent = "Annual Rain Fall " + towns[i].averageRainfall;
+                p1.textContent = "Year Founded: " + towns[i].yearFounded;
+                p2.textContent = "Current Population: " + towns[i].currentPopulation;
+                p3.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
 
                 article.appendChild(h2);
                 article.appendChild(h3);
                 article.appendChild(p1);
                 article.appendChild(p2);
                 article.appendChild(p3);
+
+                var img = document.createElement("img");
+                    if(towns[i].name == townNames[0]){
+                        img.src = "../lesson-9/images/fish_haven-_idaho_8-2011_-6842(opt).jpg";
+                    }
+                    else if(towns[i].name == townNames[2]){
+                        img.src = "../lesson-9/images/1122957169_64db139c48_b (opt).jpg";
+                    }
+                    else if(towns[i].name == townNames[1]){
+                        img.src = "../lesson-9/images/soda-springs-1768616_960_720 (opt).jpg";
+                    }
+
+                img.setAttribute("class", "jimg");
+                article.appendChild(img);
                 
                 document.getElementById("jdrop").appendChild(article);
 
